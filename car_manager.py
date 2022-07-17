481e05e7df9922420c314e1b013040e2b9964d5a
dhruv50ae <linkdp2012@gmail.com>
2022-07-17 11:42:42 +0100

    Create and move cars

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,27 @@
+from turtle import Turtle
+import random
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
+class CarManager(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.allCars = []
 
-class CarManager:
-    pass
+    def createCars(self):
+        randomChance = random.randint(1, 6)
+        if randomChance == 1:
+
+            newCar = Turtle("square")
+            newCar.shapesize(stretch_wid=1, stretch_len=2)
+            newCar.penup()
+            newCar.color(random.choice(COLORS))
+            randomY = random.randint(-250, 250)
+            newCar.goto(300, randomY)
+            self.allCars.append(newCar)
+
+    def moveCars(self):
+        for car in self.allCars:
+            car.backward(STARTING_MOVE_DISTANCE)
--- main.py
+++ main.py
@@ -9,6 +9,7 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
+carManager = CarManager()
 
 screen.listen()
 screen.onkey(player.goUp, "w")
@@ -17,3 +18,6 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    carManager.createCars()
+    carManager.moveCars()
